ddb513273b33de0b035306baf6654de7f89b0633
tonyvanriet <tonyvanriet@TvRsMBP.hsd1.il.comcast.net>
2014-11-05 00:14:45 -0600

    core's back!

---
+++ word-net/src/word_net/core.clj
@@ -0,0 +1,39 @@
+(ns word-net.core
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str])
+  (:require [word-net.digraph :as dig])
+  (:require [word-net.file-io :as fio]))
+
+
+(defn -main
+  [& args]
+  println("running main"))
+
+
+;; WordNet data type. Implement an immutable data type WordNet with the following API:
+
+;; public class WordNet {
+
+;;    // constructor takes the name of the two input files
+;;    public WordNet(String synsets, String hypernyms)
+
+;;    // returns all WordNet nouns
+;;    public Iterable<String> nouns()
+
+;;    // is the word a WordNet noun?
+;;    public boolean isNoun(String word)
+
+;;    // distance between nounA and nounB (defined below)
+;;    public int distance(String nounA, String nounB)
+
+;;    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+;;    // in a shortest ancestral path (defined below)
+;;    public String sap(String nounA, String nounB)
+
+;;    // do unit testing of this class
+;;    public static void main(String[] args)
+;; }
+
+
+(fio/get-synsets-from-file fio/synsets-file-path)
