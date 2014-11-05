5857fd544b2dd9349e14b15c0a8f78a43fac99df
tonyvanriet <tonyvanriet@TvRsMBP.hsd1.il.comcast.net>
2014-11-04 23:07:32 -0600

    read data from file and build hypernym digraph

--- word-net/src/word_net/core.clj
+++ word-net/src/word_net/core.clj
@@ -1,7 +1,8 @@
 (ns word-net.core
   (:gen-class)
   (:require [clojure.java.io :as io])
-  (:require word-net.digraph))
+  (:require [clojure.string :as str])
+  (:require [word-net.digraph :as dig]))
 
 
 (defn -main
@@ -36,7 +37,7 @@
 ;; }
 
 
-(def synsets-file-name "synsets3.txt")
+(def synsets-file-name "synsets6.txt")
 (def hypernyms-file-name "hypernyms6TwoAncestors.txt")
 
 (def file-path "src/word_net/word-net-testing/")
@@ -44,26 +45,58 @@
 (def hypernyms-file-path (str file-path hypernyms-file-name))
 
 
-
-
-(defn lazy-file-lines [file]
+(defn lazy-file-line-seq [file]
   "returns a lazy sequence of lines from the given file"
   (letfn [(helper [rdr]
                   (lazy-seq
                     (if-let [line (.readLine rdr)]
                       (cons line (helper rdr))
                       (do (.close rdr) nil))))]
-         (helper (clojure.java.io/reader file))))
-
-
+    (helper (clojure.java.io/reader file))))
 
 
 (defn get-synsets-from-file
 
   [file-name]
 
-  (doseq [line (lazy-file-lines file-name)]
-    (println "sadf" line)))
+  (loop [synsets-file-lines (lazy-file-line-seq file-name)
+         synsets []]
+    (if (empty? synsets-file-lines)
+      synsets
+      (do
+        ;(println (first synsets-file-lines))
+        (let [[id nouns-raw gloss] (str/split (first synsets-file-lines) #",")]
+          (let [nouns (str/split nouns-raw #" ")]
+            (recur (rest synsets-file-lines)
+                   (conj synsets {:id id :nouns nouns}))))))))
 
 
 (get-synsets-from-file synsets-file-path)
+
+
+(defn get-hypernym-digraph-from-file
+
+  [file-name]
+
+  (loop [hypernyms-file-lines (lazy-file-line-seq file-name)
+         digraph (dig/build-new-digraph)]
+
+    (if (empty? hypernyms-file-lines)
+
+      digraph
+
+      (do
+        (let [ids (str/split (first hypernyms-file-lines) #",")
+              synset-id (first ids)
+              hypernym-ids (rest ids)]
+
+          (def digraph-after-edges
+            (reduce (fn [digraph hypernym-id]
+                      (dig/add-edge synset-id hypernym-id digraph))
+                    digraph
+                    hypernym-ids))
+
+          (recur (rest hypernyms-file-lines) digraph-after-edges))))))
+
+
+(get-hypernym-digraph-from-file hypernyms-file-path)
